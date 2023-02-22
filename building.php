5961304a69e48ee71d25c6bc489ab3d873edf07d
Hammad <qeensnkings@gmail.com>
2023-02-22 13:01:54 +0530

    yes

---
+++ building.php
@@ -0,0 +1,134 @@
+<?php
+    
+?>
+
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.0.0/dist/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
+    <title>Parking System</title>
+    
+</head>
+<body>
+    <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+        <!-- Building name -->
+        <a class="navbar-brand" href="#">Building 1</a>                         
+        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+          <span class="navbar-toggler-icon"></span>
+        </button>
+      
+        <div class="collapse navbar-collapse" id="navbarSupportedContent">
+          <ul class="navbar-nav mr-auto">
+            <li class="nav-item dropdown">
+              <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                Floors
+              </a>
+              <div class="dropdown-menu" aria-labelledby="navbarDropdown">
+                <a class="dropdown-item" href="#">Floor 1</a>
+                <a class="dropdown-item" href="#">Floor 2</a>
+              
+              </div>
+            </li>
+            <li class="nav-item " style="margin-left:1100px ;">
+              <a class="nav-link " href="#">Price : 100/hr</a>
+            </li>
+          </ul>
+        </div>
+    </nav>
+
+    <main>
+        <div style="padding: 20px;"><strong>Floor 1 </strong></div>
+        <div class="container"  style="display: flex;">
+            <div style="border: 1px solid black; margin: 20px;">
+                <img src="#" alt="" style="padding: 50px;">
+            </div>
+            <div style="border: 1px solid black; margin: 20px;">
+                <img src="#" alt="" style="padding: 50px;">
+            </div>
+            <div style="border: 1px solid black; margin: 20px;">
+                <img src="#" alt="" style="padding: 50px;">
+            </div>
+            <div style="border: 1px solid black; margin: 20px;">
+                <img src="#" alt="" style="padding: 50px;">
+            </div>
+            <div style="border: 1px solid black; margin: 20px;">
+                <img src="#" alt="" style="padding: 50px;">
+            </div>
+            <div style="border: 1px solid black; margin: 20px;">
+                <img src="#" alt="" style="padding: 50px;">
+            </div>
+        </div>
+        <div class="container"  style="display: flex;">
+            <div style="border: 1px solid black; margin: 20px;">
+                <img src="#" alt="" style="padding: 50px;">
+            </div>
+            <div style="border: 1px solid black; margin: 20px;">
+                <img src="#" alt="" style="padding: 50px;">
+            </div>
+            <div style="border: 1px solid black; margin: 20px;">
+                <img src="#" alt="" style="padding: 50px;">
+            </div>
+            <div style="border: 1px solid black; margin: 20px;">
+                <img src="#" alt="" style="padding: 50px;">
+            </div>
+            <div style="border: 1px solid black; margin: 20px;">
+                <img src="#" alt="" style="padding: 50px;">
+            </div>
+            <div style="border: 1px solid black; margin: 20px;">
+                <img src="#" alt="" style="padding: 50px;">
+            </div>
+        </div>
+
+        <div style="padding: 20px;"><strong>Floor 1 </strong></div>
+        <div class="container"  style="display: flex;">
+            <div style="border: 1px solid black; margin: 20px;">
+                <img src="#" alt="" style="padding: 50px;">
+            </div>
+            <div style="border: 1px solid black; margin: 20px;">
+                <img src="#" alt="" style="padding: 50px;">
+            </div>
+            <div style="border: 1px solid black; margin: 20px;">
+                <img src="#" alt="" style="padding: 50px;">
+            </div>
+            <div style="border: 1px solid black; margin: 20px;">
+                <img src="#" alt="" style="padding: 50px;">
+            </div>
+            <div style="border: 1px solid black; margin: 20px;">
+                <img src="#" alt="" style="padding: 50px;">
+            </div>
+            <div style="border: 1px solid black; margin: 20px;">
+                <img src="#" alt="" style="padding: 50px;">
+            </div>
+        </div>
+        <div class="container"  style="display: flex;">
+            <div style="border: 1px solid black; margin: 20px;">
+                <img src="#" alt="" style="padding: 50px;">
+            </div>
+            <div style="border: 1px solid black; margin: 20px;">
+                <img src="#" alt="" style="padding: 50px;">
+            </div>
+            <div style="border: 1px solid black; margin: 20px;">
+                <img src="#" alt="" style="padding: 50px;">
+            </div>
+            <div style="border: 1px solid black; margin: 20px;">
+                <img src="#" alt="" style="padding: 50px;">
+            </div>
+            <div style="border: 1px solid black; margin: 20px;">
+                <img src="#" alt="" style="padding: 50px;">
+            </div>
+            <div style="border: 1px solid black; margin: 20px;">
+                <img src="#" alt="" style="padding: 50px;">
+            </div>
+        </div>
+
+    </main>
+    <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
+<script src="https://cdn.jsdelivr.net/npm/popper.js@1.12.9/dist/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@4.0.0/dist/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
+    
+</body>
+</html>
